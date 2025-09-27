497849a0739a595e7ebf53b122c3e4b47a3a0280
ankur-JA <rajankur1802@gmail.com>
2025-09-27 06:07:12 +0530

    deployment script

--- packages/foundry/script/DeployDCAScheduler.s.sol
+++ packages/foundry/script/DeployDCAScheduler.s.sol
@@ -1,28 +1,28 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/Script.sol";
+import "./DeployHelpers.s.sol";
 import "../contracts/DCAScheduler.sol";
 
 /**
  * @title DeployDCAScheduler
  * @dev Deployment script for DCAScheduler contract
+ * @author Gearhead
  */
-contract DeployDCAScheduler is Script {
-    function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address feeRecipient = vm.envAddress("FEE_RECIPIENT");
+contract DeployDCAScheduler is ScaffoldETHDeploy {
+    function run() external ScaffoldEthDeployerRunner {
+        // Set fee recipient (default to deployer if not set)
+        address feeRecipient = deployer;
         
-        vm.startBroadcast(deployerPrivateKey);
-
         // Deploy DCAScheduler contract
         DCAScheduler dcaScheduler = new DCAScheduler(feeRecipient);
-
-        vm.stopBroadcast();
+        
+        // Add to deployments array for export
+        deployments.push(Deployment("DCAScheduler", address(dcaScheduler)));
 
         // Log deployment information
         console.log("DCAScheduler deployed at:", address(dcaScheduler));
         console.log("Fee recipient:", feeRecipient);
-        console.log("Deployer:", vm.addr(deployerPrivateKey));
+        console.log("Deployer:", deployer);
     }
 }
